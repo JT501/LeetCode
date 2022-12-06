1e9567b2fedcf07bb05fad4f4b6ee7d22086e7d2
Johnny Tsoi <kiucheongchoi@gmail.com>
2022-12-06 19:10:46 +0800

    797

--- LeetCode.playground/Pages/797. All Paths From Source to Target .xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/797. All Paths From Source to Target .xcplaygroundpage/Contents.swift
@@ -1,10 +1,41 @@
 /*:
- ## Breath First Search
- ### Time: O(N)
+ ## Backtracking
+ ### Time: O(N*2^N)
  ### Space: O(N)
  ![submission](1.png)
  */
 class Solution {
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        guard !graph.isEmpty else { return [] }
+        
+        let target = graph.count - 1
+        
+        var stack: [Int] = []
+        var result: [[Int]] = []
+        
+        func dfs(n: Int) {
+            guard n != target else {
+                result.append(stack + [target])
+                return
+            }
+            for i in graph[n] {
+                stack.append(n)
+                dfs(n: i)
+                stack.removeLast()
+            }
+        }
+        dfs(n: 0)
+        return result
+    }
+}
+
+/*:
+ ## Breath First Search
+ ### Time: O(N*2^N)
+ ### Space: O(N)
+ ![submission](2.png)
+ */
+class Solution2 {
     func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
         var queue = [(n: Int, path: [Int])]()
         var target = graph.count - 1
@@ -33,18 +64,18 @@ class Solution {
     }
 }
 
-let s = Solution().allPathsSourceTarget
+let s2 = Solution2().allPathsSourceTarget
 
-s([[1,2],[3],[3],[]])
-s([[4,3,1],[3,2,4],[3],[4],[]])
+s2([[1,2],[3],[3],[]])
+s2([[4,3,1],[3,2,4],[3],[4],[]])
 
 /*:
  ## Depth First Search
- ### Time: O(N)
+ ### Time: O(N*2^N)
  ### Space: O(N)
- ![submission](2.png)
+ ![submission](3.png)
  */
-class Solution2 {
+class Solution3 {
     func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
         var target = graph.count - 1
         var ans = [[Int]]()
@@ -69,7 +100,7 @@ class Solution2 {
     }
 }
 
-let s2 = Solution2().allPathsSourceTarget
+let s3 = Solution3().allPathsSourceTarget
 
-s2([[1,2],[3],[3],[]])
-s2([[4,3,1],[3,2,4],[3],[4],[]])
+s3([[1,2],[3],[3],[]])
+s3([[4,3,1],[3,2,4],[3],[4],[]])
