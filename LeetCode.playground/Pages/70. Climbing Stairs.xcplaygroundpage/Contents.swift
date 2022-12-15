2dce226c6ad4f57db5f8a94eeef1a4691f195f0b
Johnny Tsoi <kiucheongchoi@gmail.com>
2022-12-15 19:20:13 +0800

    Solved 70

--- LeetCode.playground/Pages/70. Climbing Stairs.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/70. Climbing Stairs.xcplaygroundpage/Contents.swift
@@ -1,10 +1,67 @@
 /*:
- ## Dynamic Programming + Memoization
+ ## Dynamic Programming (Bottom-Up + Space Optimized)
+ ### Time: O(N)
+ ### Space: O(1)
+ ![submission](1.png)
+ */
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n <= 2 { return n }
+        
+        var prev1 = 2
+        var prev2 = 1
+        
+        for _ in 3...n {
+            let temp = prev1
+            prev1 = prev1 + prev2
+            prev2 = temp
+        }
+        
+        return prev1
+    }
+}
+
+let s = Solution().climbStairs
+
+s(5)
+s(38)
+
+/*:
+ ## Dynamic Programming (Bottom-Up)
+ ### Time: O(N)
+ ### Space: O(N)
+ ![submission](2.png)
+ */
+class Solution2 {
+    func climbStairs(_ n: Int) -> Int {
+        if n <= 2 { return n }
+        
+        var memo = Array(repeating: 0, count: n+1)
+
+        memo[1] = 1
+        memo[2] = 2
+        
+        for i in 3...n {
+            memo[i] = memo[i-1] + memo[i-2]
+        }
+        
+        return memo[n]
+    }
+}
+
+let s2 = Solution2().climbStairs
+
+s2(5)
+s2(38)
+
+
+/*:
+ ## Dynamic Programming (Top-Down)
  ### Time: O(N)
  ### Space: O(N)
  ![submission](1.png)
  */
-class Solution {
+class Solution3 {
     var memo: [Int: Int] = [:]
     
     func climbStairs(_ n: Int) -> Int {
@@ -23,7 +80,7 @@ class Solution {
     }
 }
 
-let s = Solution()
+let s3 = Solution3()
 
-s.climbStairs(5)
-s.climbStairs(38)
+s3.climbStairs(5)
+s3.climbStairs(38)
