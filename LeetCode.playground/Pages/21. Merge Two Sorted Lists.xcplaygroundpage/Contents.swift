6e593010e4834f22d41ce7af77fb44afdb709e2b
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-04 17:15:14 +0800

    Solved 21 again

--- LeetCode.playground/Pages/21. Merge Two Sorted Lists.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/21. Merge Two Sorted Lists.xcplaygroundpage/Contents.swift
@@ -1,21 +1,22 @@
 public class ListNode {
     public var val: Int
     public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    public init() { val = 0; next = nil }
+    public init(_ val: Int) { self.val = val; next = nil }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
 }
 
 /*:
- ### Time: O(N+M)
- ### Space: O(1)
+ ## Recursion
+ ### Time: O(n+m)
+ ### Space: O(n+m)
  ![submission](1.png)
  */
 class Solution {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        guard let list1 = list1 else { return list2 }
-        guard let list2 = list2 else { return list1 }
-        
+        guard let list1 else { return list2 }
+        guard let list2 else { return list1 }
+
         if list1.val <= list2.val {
             list1.next = mergeTwoLists(list1.next, list2)
             return list1
@@ -32,38 +33,36 @@ s.mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, L
 
 
 /*:
- ### Time: O(N+M)
+ ## Iteration
+ ### Time: O(n+m)
  ### Space: O(1)
  ![submission](2.png)
  */
 class Solution2 {
     func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        
-        func merge(_ list1: ListNode?, _ list2: ListNode?, _ previous: ListNode, _ head: ListNode) -> ListNode? {
-            if list1 == nil && list2 == nil {
-                return head.next
-            } else if list1 == nil && list2 != nil {
-                previous.next = list2
-                return head.next
-            } else if list2 == nil && list1 != nil {
-                previous.next = list1
-                return head.next
-            }
-            
-            if list1!.val <= list2!.val {
-                var next = list1!.next
-                previous.next = ListNode(list1!.val)
-                return merge(next, list2, previous.next!, head)
+        var list1 = list1, list2 = list2
 
+        let dummy = ListNode()
+        var curr = dummy
+
+        while list1 != nil || list2 != nil {
+            if list1 == nil {
+                curr.next = list2
+                break
+            } else if list2 == nil {
+                curr.next = list1
+                break
+            } else if list1!.val < list2!.val {
+                curr.next = list1
+                list1 = list1?.next
             } else {
-                var next = list2!.next
-                previous.next = ListNode(list2!.val)
-                return merge(list1, next, previous.next!, head)
+                curr.next = list2
+                list2 = list2?.next
             }
+            curr = curr.next!
         }
-        
-        var head = ListNode()
-        return merge(list1, list2, head, head)
+
+        return dummy.next
     }
 }
 
