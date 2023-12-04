47f5620b1c08223382d9c94b877306b402a535a7
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-04 15:24:11 +0800

    Solved 206 again

--- LeetCode.playground/Pages/206. Reverse Linked List.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/206. Reverse Linked List.xcplaygroundpage/Contents.swift
@@ -1,34 +1,94 @@
 public class ListNode {
     public var val: Int
     public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+    public init() { val = 0; next = nil }
+    public init(_ val: Int) { self.val = val; next = nil }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next }
 }
 
 /*:
- ### Time: O(N)
- ### Space: O(1)
+ ### Time: O(n)
+ ### Space: O(n)
  ![submission](1.png)
  */
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-        
-        func reverse(_ head: ListNode?, _ previous: ListNode?) -> ListNode? {
-            if let head = head {
-                var next = head.next
-                head.next = previous
-                
-                return reverse(next, head)
-            } else {
-                return previous
-            }
-        }
-        
-        return reverse(head, nil)
+        guard head != nil && head?.next != nil else { return head }
+
+        let p = reverseList(head?.next)
+        head?.next?.next = head
+        head?.next = nil
+
+        return p
     }
 }
 
 let s = Solution()
 
 s.reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+
+/*:
+ ### Time: O(n)
+ ### Space: O(n)
+ ![submission](2.png)
+ */
+class Solution2 {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        func reverse(head: ListNode?, prev: ListNode?) -> ListNode? {
+            guard let head else { return prev }
+
+            let next = head.next
+            head.next = prev
+
+            return reverse(head: next, prev: head)
+        }
+
+        return reverse(head: head, prev: nil)
+    }
+}
+
+/*:
+ ### Time: O(n)
+ ### Space: O(1)
+ ![submission](3.png)
+ */
+class Solution3 {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode?
+        var current = head
+
+        while current != nil {
+            let next: ListNode? = current!.next
+            current!.next = prev
+            prev = current
+            current = next
+        }
+
+        return prev
+    }
+}
+
+/*:
+ ### Time: O(n)
+ ### Space: O(n)
+ ![submission](4.png)
+ */
+class Solution4 {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var values = [Int]()
+        var node = head
+
+        while node != nil {
+            values.append(node!.val)
+            node = node!.next
+        }
+
+        node = head
+        while node != nil {
+            node!.val = values.removeLast()
+            node = node!.next
+        }
+
+        return head
+    }
+}
