5246d994344313af0e38b8c9d5dbbc3163554e06
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-10 19:17:52 +0800

    Update 224

--- LeetCode.playground/Pages/224. Basic Calculator.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/224. Basic Calculator.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 /*:
  ## Stack
- ### Time: O(N)
- ### Space: O(N)
+ ### Time: O(n)
+ ### Space: O(n)
  ![submission](1.png)
  */
 class Solution {
@@ -11,36 +11,61 @@ class Solution {
         var number = 0
         var sign = 1
         var result = 0
-        
-        for char in s {
-            switch char {
-            case "0"..."9":
-                number = (number * 10 + Int("\(char)")!)
-            case "+":
-                result += number * sign
-                number = 0
-                sign = 1
-            case "-":
-                result += number * sign
-                sign = -1
-                number = 0
-            case "(":
-                stack.push(result)
-                stack.push(sign)
-                sign = 1
-                result = 0
-                number = 0
-            case ")":
-                result += number * sign
-                result = result * stack.pop()! + stack.pop()!
-                number = 0
-                sign = 1
-            default:
-                break
+
+        for c in s {
+            switch c {
+                case "0"..."9":
+                    number = number * 10 + (Int(String(c)) ?? 0)
+                case "+":
+                    result += number * sign
+                    number = 0
+                    sign = 1
+                case "-":
+                    result += number * sign
+                    number = 0
+                    sign = -1
+                case "(":
+                    stack.push(result)
+                    stack.push(sign)
+                    result = 0
+                    number = 0
+                    sign = 1
+                case ")":
+                    result += number * sign
+                    result = stack.pop()! * result + stack.pop()!
+                    number = 0
+                    sign = 1
+                default:
+                    break
             }
         }
-        
+
         return result + number * sign
+    }
+
+    class Stack<T> {
+        private var elements: [T] = []
+        var isEmpty: Bool {
+            elements.isEmpty
+        }
+
+        init() {}
+
+        func peek() -> T? {
+            elements.last
+        }
+
+        func push(_ value: T) {
+            elements.append(value)
+        }
+
+        func pop() -> T? {
+            elements.removeLast()
+        }
+
+        func toArray() -> [T] {
+            elements
+        }
     }
 }
 
@@ -52,3 +77,50 @@ s("(1+(4+5+2)-3)+(6+8)")
 s("- (3 + (4 + 5))")
 s("1-11")
 s("2-4-(8+2-6+(8+4-(1)+8-10))")
+
+/*:
+ ## Recursion
+ ### Time: O(n)
+ ### Space: O(n)
+ ![submission](2.png)
+ */
+class Solution2 {
+    func calculate(_ s: String) -> Int {
+        var s = Array(s)
+        var idx = 0
+
+        return cal(s, &idx)
+    }
+
+    func cal(_ str: [Character], _ idx: inout Int) -> Int {
+        var number = 0
+        var sign = 1
+        var result = 0
+
+        outerloop: while idx < str.count {
+            let char = str[idx]
+            
+            switch char {
+                case "0"..."9":
+                    number = number * 10 + (Int(String(char)) ?? 0)
+                case "+":
+                    result += number * sign
+                    number = 0
+                    sign = 1
+                case "-":
+                    result += number * sign
+                    number = 0
+                    sign = -1
+                case "(":
+                    idx += 1
+                    result += sign * cal(str, &idx)
+                case ")":
+                    break outerloop
+                default:
+                    break
+            }
+            idx += 1
+        }
+        return result + number * sign
+    }
+}
