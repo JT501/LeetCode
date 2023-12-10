2be59be632821ccc8492ca2731ed5f576f23a14a
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-11 01:15:57 +0800

    Solved 739 again

--- LeetCode.playground/Pages/739. Daily Temperatures .xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/739. Daily Temperatures .xcplaygroundpage/Contents.swift
@@ -6,27 +6,51 @@
  */
 class Solution {
     func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        var monoStack = [Int]()
+        var monoStack = Stack<Int>()
         var ans = Array(repeating: 0, count: temperatures.count)
-        
-        for (i, temperature) in temperatures.enumerated() {
-            if monoStack.isEmpty || temperature < temperatures[monoStack.last!] {
-                monoStack.append(i)
+
+        for (i, t) in temperatures.enumerated() {
+            if monoStack.isEmpty || t <= temperatures[monoStack.peek()!] {
+                monoStack.push(i)
             } else {
-                while !monoStack.isEmpty && temperature > temperatures[monoStack.last!] {
-                    let lastIndex = monoStack.removeLast()
-                    ans[lastIndex] = i - lastIndex
+                while !monoStack.isEmpty, t > temperatures[monoStack.peek()!] {
+                    ans[monoStack.peek()!] = i - monoStack.pop()!
                 }
-                monoStack.append(i)
+                monoStack.push(i)
             }
         }
-        
+
         return ans
+    }
+
+    class Stack<T> {
+        private var elements: [T] = []
+        var isEmpty: Bool {
+            elements.isEmpty
+        }
+
+        init() {}
+
+        func peek() -> T? {
+            elements.last
+        }
+
+        func push(_ value: T) {
+            elements.append(value)
+        }
+
+        func pop() -> T? {
+            elements.removeLast()
+        }
+
+        func toArray() -> [T] {
+            elements
+        }
     }
 }
 
 let s = Solution().dailyTemperatures
 
-s([73,74,75,71,69,72,76,73])
-s([30,40,50,60])
-s([30,60,90])
+s([73, 74, 75, 71, 69, 72, 76, 73])
+s([30, 40, 50, 60])
+s([30, 60, 90])
