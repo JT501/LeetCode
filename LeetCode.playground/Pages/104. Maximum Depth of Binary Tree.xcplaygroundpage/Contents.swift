65c6d1e97a06e883da6d1a84ef8e00e0256635fa
Johnny Tsoi <kiucheongchoi@gmail.com>
2023-12-15 23:27:28 +0800

    Solved 104

--- LeetCode.playground/Pages/104. Maximum Depth of Binary Tree.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/104. Maximum Depth of Binary Tree.xcplaygroundpage/Contents.swift
@@ -1,3 +1,49 @@
+/*:
+ ## Binary Tree, Recursion
+ ### Time: O(n)
+ ### Space: O(㏒n)
+ ![submission](1.png)
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+
+        return 1 + max(maxDepth(root.left), maxDepth(root.right))
+    }
+}
+
+let s = Solution()
+
+s.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
+s.maxDepth(TreeNode(1, nil, TreeNode(2)))
+
+class Solution2 {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        var stack = [(Int, TreeNode?)]()
+
+        stack.append((1, root))
+        var depth = 0
+
+        while !stack.isEmpty {
+            let (current_depth, _root) = stack.removeLast()
+
+            if let _root = _root {
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, _root.left))
+                stack.append((current_depth + 1, _root.right))
+            }
+        }
+
+        return depth
+    }
+}
+
+let s2 = Solution2()
+
+s2.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
+s2.maxDepth(TreeNode(1, nil, TreeNode(2)))
+
 public class TreeNode {
     public var val: Int
     public var left: TreeNode?
@@ -10,23 +56,3 @@ public class TreeNode {
         self.right = right
     }
 }
-
-class Solution {
-    func maxDepth(_ root: TreeNode?) -> Int {
-        
-        func dig(_ root: TreeNode?, count: Int) -> Int {
-            if let root = root {
-                return max(dig(root.left, count: count + 1), dig(root.right, count: count + 1))
-            } else {
-                return count
-            }
-        }
-        
-        return dig(root, count: 0)
-    }
-}
-
-let s = Solution()
-
-s.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
-s.maxDepth(TreeNode(1, nil, TreeNode(2)))
