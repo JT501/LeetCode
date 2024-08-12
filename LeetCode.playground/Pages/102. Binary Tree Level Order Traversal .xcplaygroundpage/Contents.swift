b37078600f07743536ddd271546709ab147089d2
Johnny Tsoi <kiucheongchoi@gmail.com>
2024-08-12 16:26:19 -0700

    Solved 102

--- LeetCode.playground/Pages/102. Binary Tree Level Order Traversal .xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/102. Binary Tree Level Order Traversal .xcplaygroundpage/Contents.swift
@@ -1,7 +1,40 @@
-//: [Previous](@previous)
+/*:
+ ## Tree, Breadth First Search
+ ### Time: O(N)
+ ### Space: O(N)
+ ![submission](1.png)
+ */
 
-import Foundation
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root else { return [] }
 
-var greeting = "Hello, playground"
+        var queue = Queue([(root, 0)])
+        var ans = [[Int]]()
+        var temp = [Int]()
+        var curLevel = 0
 
-//: [Next](@next)
+        while !queue.isEmpty {
+            let (node, level) = queue.pop()!
+            
+            if curLevel < level && !temp.isEmpty {
+                ans.append(temp)
+                temp.removeAll()
+            }
+
+            curLevel = level
+            temp.append(node.val)
+
+            if let left = node.left {
+                queue.push((left, level + 1))
+            }
+            if let right = node.right {
+                queue.push((right, level + 1))
+            }
+        }
+
+        ans.append(temp)
+
+        return ans
+    }
+}
